1f7dae4820e43416cbdc67335457ab0b78f65dc7
N1ck <cbnickp@gmail.com>
2019-10-30 23:02:25 +0700

    Update users.js

--- db/models/users.js
+++ db/models/users.js
@@ -56,6 +56,10 @@ module.exports = (sequelize) => {
         },
         notEmpty: {
           msg: 'Please provide a value for "password"'
+        },
+        len: {
+          args: [8, 16],
+          msg: "Email address must be between 8 and 16 characters in length"
         }
       }
     }
